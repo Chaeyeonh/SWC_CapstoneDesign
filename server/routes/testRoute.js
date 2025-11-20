7cb57f9e29b7e93b481580ff18f5d2750cb4936b
Chaeyeonh <strawberry@khu.ac.kr>
2025-11-20 21:10:34 +0900

    feat: preset 테스트 및 UI 표시

--- server/routes/testRoute.js
+++ server/routes/testRoute.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const { runTest } = require("../controllers/testController");
+const { runScreening } = require("../controllers/screeningController");
 
 const router = express.Router();
+
+// health check
 router.get("/", (req, res) => {
   res.send("OK");
 });
 
+// test run
 router.post("/", runTest);
+
+// screening
+router.post("/screening", runScreening);
 
 module.exports = router;
